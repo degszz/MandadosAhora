012fa56ad0b43a54b9cf1f448fcf7c4e62794995
degsz <125046749+degszz@users.noreply.github.com>
2025-12-06 04:48:15 -0300

    feat(tailwindconf-styles): add fonts

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: "class",
+    content: [
+        "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
